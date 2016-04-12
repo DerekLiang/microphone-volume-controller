6113dac82c6e16fa1f7a8353cd86193864a20a2a
Derek Liang <derek.liang@pearson.com>
2016-04-12 12:53:07 -0700

    refactoring

--- VolumeControl/App.xaml.cs
+++ VolumeControl/App.xaml.cs
@@ -18,10 +18,5 @@ namespace VolumeControl
     public partial class App : Application
     {
 
-
-        private void Application_Startup(object sender, StartupEventArgs e)
-        {
-
-        }
     }
 }
